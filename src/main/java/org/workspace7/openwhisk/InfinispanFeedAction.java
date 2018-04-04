b2d74c441410f94f3c7c6ef1ac610aa9a72d69d2
Kamesh Sampath <kamesh.sampath@hotmail.com>
2018-04-04 17:52:00 +0530

    Making feedActionURL as request parameter

--- src/main/java/org/workspace7/openwhisk/InfinispanFeedAction.java
+++ src/main/java/org/workspace7/openwhisk/InfinispanFeedAction.java
@@ -1,6 +1,10 @@
 package org.workspace7.openwhisk;
 
+import java.nio.charset.Charset;
+import java.util.Base64;
+
 import com.google.gson.JsonObject;
+
 import feign.Feign;
 import feign.Headers;
 import feign.Param;
@@ -10,22 +14,20 @@ import feign.gson.GsonEncoder;
 import feign.okhttp.OkHttpClient;
 import feign.slf4j.Slf4jLogger;
 
-import java.nio.charset.Charset;
-import java.util.Base64;
-
-
 public class InfinispanFeedAction {
 
   public static JsonObject main(JsonObject request) {
     JsonObject response = new JsonObject();
-    //TODO validation
+    /* This can be made as function level defauly parameter when creating 
+    * the function, so that the user need not send it every time as part of the request
+    * sending as part of the request will overirde the default
+    */
+    String feedActionURL = request.get("feedActionURL").getAsString();
+
     try {
-      InfinispanProvider provider = Feign.builder()
-        .client(new OkHttpClient())
-        .decoder(new GsonDecoder())
-        .encoder(new GsonEncoder())
-        .logger(new Slf4jLogger(InfinispanProvider.class))
-        .target(InfinispanProvider.class, "http://infinispan-feed-provider.myproject:8080");
+      InfinispanProvider provider = Feign.builder().client(new OkHttpClient()).decoder(new GsonDecoder())
+          .encoder(new GsonEncoder()).logger(new Slf4jLogger(InfinispanProvider.class))
+          .target(InfinispanProvider.class, feedActionURL);
       response.addProperty("done", true);
       if (request.get("lifecycleEvent").getAsString().equalsIgnoreCase("CREATE")) {
         System.out.println("CREATE TRIGGER");
@@ -33,8 +35,8 @@ public class InfinispanFeedAction {
         System.out.println("RESPONSE:" + response);
       } else if (request.get("lifecycleEvent").getAsString().equalsIgnoreCase("DELETE")) {
         System.out.println("DELETE TRIGGER:");
-        String triggerName = new String(Base64.getEncoder().encode(request.get("triggerName")
-          .getAsString().getBytes(Charset.forName("US-ASCII"))));
+        String triggerName = new String(
+            Base64.getEncoder().encode(request.get("triggerName").getAsString().getBytes(Charset.forName("US-ASCII"))));
         response = provider.deleteListenerFromTrigger(triggerName);
       }
     } catch (Exception e) {
@@ -43,7 +45,6 @@ public class InfinispanFeedAction {
     }
     return response;
   }
-
 
   interface InfinispanProvider {
     @RequestLine("POST /api/feed/listener")
